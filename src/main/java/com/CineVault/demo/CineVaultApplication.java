38a9947a4c72c8378e1fdfdf9a3896dfd562886b
SaitejaKommi <kommisaiteja677@gmail.com>
2026-02-18 09:37:36 +0530

    Initial commit

---
+++ src/main/java/com/CineVault/demo/CineVaultApplication.java
@@ -0,0 +1,13 @@
+package com.CineVault.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineVaultApplication.class, args);
+	}
+
+}
